31aca728d0d1ac8db1504d64eedb76eac607f117
Alex Morehead <alex.morehead@gmail.com>
2021-01-26 09:58:08 -0600

    Add new configs

--- home/xsetwacom.sh
+++ home/xsetwacom.sh
@@ -1,2 +1,3 @@
 xsetwacom --set "Wacom Intuos S Pen stylus" Button 2 "pan"
 xsetwacom --set "Wacom Intuos S Pen stylus" "PanScrollThreshold" 100
+xsetwacom --set "Wacom Intuos S Pad pad" Mode "Relative"
