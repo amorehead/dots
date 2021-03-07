4760f9d7ba0a3358de60c0a0321cf8af2808e25b
Alex Morehead <alex.morehead@gmail.com>
2021-03-07 09:31:58 -0600

    Update xsetwacom autostart script to set stylus rather than touchpad to relative mode

--- home/xsetwacom.sh
+++ home/xsetwacom.sh
@@ -1,3 +1,3 @@
 xsetwacom --set "Wacom Intuos S Pen stylus" Button 2 "pan"
 xsetwacom --set "Wacom Intuos S Pen stylus" "PanScrollThreshold" 100
-xsetwacom --set "Wacom Intuos S Pad pad" Mode "Relative"
+xsetwacom --set "Wacom Intuos S Pen stylus" Mode "Relative"
